dac972b66dc011acf6a437bf0f992b7c9a5921f8
qibal <iqbalherlambang.39@gmail.com>
2023-12-23 18:46:23 +0700

    fixed tailwindcss

--- resources/views/user/HomeUser.blade.php
+++ resources/views/user/HomeUser.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DDE3E</title>
-
-    @vite('css/tailwind.css')
+    {{-- <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet"> --}}
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1 class="text-red-100">tes tailwind laravel</h1>
-    <h2 class="text-red-500">tes</h2>
+    <h1 class="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/views/admin/**/*.blade.php",
-    "./resources/views/user/**/*.blade.php",
-    "./resources/views/admin/**/*.js",
-    "./resources/views/user/**/*.js",
-    "./resources/views/admin/**/*.vue",
-    "./resources/views/user/**/*.vue",
+    "./resources/**/*.{blade.php,js.vue}",
+    // "./resources/**/*.blade.php",
+    // "./resources/**/*.js",
+    // "./resources/**/*.vue",
   ],
   theme: {
     extend: {},
