9ee56d9b1d22c83f606fd03407091f9c15e116f6
qibal <iqbalherlambang.39@gmail.com>
2023-12-26 16:42:43 +0700

    finsih crud fakultas, 1

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/fakultas',[fakultasController::class,'fakultasRead'])->name('fakult
 Route::post('/fakultas-Add',[fakultasController::class,'fakultasAdd'])->name('fakultasAdd');
 Route::get('/fakultas-Delete/{id}',[fakultasController::class,'fakultasDelete'])->name('fakultasDelete');
 Route::get('/fakultas-Edit/{id}',[fakultasController::class,'fakultasEdit'])->name('fakultasEdit');
-Route::post('/fakultas-Update',[fakultasController::class,'fakultasUpdate'])->name('fakultasUpdate');
+Route::post('/fakultas-Update/{id}',[fakultasController::class,'fakultasUpdate'])->name('fakultasUpdate');
 
 // user login
 Route::get('/login',[loginController::class,'login'])->name('login');
